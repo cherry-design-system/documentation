db6fdda29d84e729c36df70eec7f166e41a8848d
Luan Gjokaj <luan@riangle.com>
2021-01-07 20:59:12 +0100

    feat: gtm

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,19 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
-				<Head />
+				<Head>
+					{process.env.DISABLE_GTM === "false" && (
+						<script
+							dangerouslySetInnerHTML={{
+								__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+							})(window,document,'script','dataLayer','GTM-K8M4PWZ');`,
+							}}
+						></script>
+					)}
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
