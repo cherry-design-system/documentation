417b748f766330ea07c8f8b2dd4ae8de937e323a
Luan Gjokaj <luan@riangle.com>
2021-01-31 15:18:58 +0100

    chore: update dependencies

--- src/components/Layout/Label/Label.js
+++ src/components/Layout/Label/Label.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { labelStyles } from "./Label.styles";
 
-function Label({ className, children, error, success, ...props }) {
+function Label({ className, children, error, success, htmlFor, ...props }) {
 	return (
 		<label
 			className={className}
 			css={(theme) => labelStyles(theme, error, success)}
+			htmlFor={htmlFor}
 			{...props}
 		>
 			{children}
