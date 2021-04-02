f7d4a9f43361ca371fe0d26b8390ff46ade26e41
Luan Gjokaj <luan@riangle.com>
2021-04-02 12:28:11 +0200

    feat: preload fonts

--- src/assets/styles/globalStyles.js
+++ src/assets/styles/globalStyles.js
@@ -5,8 +5,6 @@ import { theme } from "./theme";
 const globalStyles = (
 	<Global
 		styles={css`
-			@import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap");
-
 			html,
 			body {
 				margin: 0;
