ff9298a910cbf370b9902d8e7233483bbeb7da4e
Luan <luan@riangle.com>
2021-04-05 02:45:15 +0200

    refactor: spacing

--- src/pages/docs/design/inputs.js
+++ src/pages/docs/design/inputs.js
@@ -361,7 +361,7 @@ function Index({ posts = [] }) {
 						id="slide-big-1"
 						size="big"
 					/>
-					<Space size={20} />
+					<Space size={25} />
 				</Col>
 				<Col xs={12} lg={4}>
 					<ToggleInput
@@ -373,7 +373,7 @@ function Index({ posts = [] }) {
 						checked
 						size="big"
 					/>
-					<Space size={20} />
+					<Space size={25} />
 				</Col>
 				<Col xs={12} lg={4}>
 					<ToggleInput
@@ -384,20 +384,20 @@ function Index({ posts = [] }) {
 						disabled
 						size="big"
 					/>
-					<Space size={20} />
+					<Space size={25} />
 				</Col>
 
 				<Col xs={12} lg={4}>
 					<RangeSlider label="Toggle" />
-					<Space size={20} />
+					<Space size={25} />
 				</Col>
 				<Col xs={12} lg={4}>
 					<RangeSlider label="Toggle" size="big" />
-					<Space size={20} />
+					<Space size={25} />
 				</Col>
 				<Col xs={12} lg={4}>
 					<RangeSlider label="Toggle" size="big" disabled />
-					<Space size={20} />
+					<Space size={25} />
 				</Col>
 
 				<Col xs={12} lg={4}>
