b845e4488fd6b760408cbade8e5654c3a6b00aba
Luan Gjokaj <luan@riangle.com>
2021-04-09 14:14:00 +0200

    fix: load fonts once

--- src/components/Pages/Page.js
+++ src/components/Pages/Page.js
@@ -49,28 +49,6 @@ const Page = ({
 			<link rel="icon" type="image/png" sizes="16x16" href={favIcon16} />
 			<link rel="shortcut icon" href={favIcon} />
 			<title>{title + " - Cherry"}</title>
-			<link
-				rel="preconnect"
-				href="https://fonts.gstatic.com"
-				crossOrigin="true"
-			/>
-			<link
-				rel="preload"
-				as="style"
-				href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-			/>
-			<link
-				rel="stylesheet"
-				href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-				media="print"
-				onLoad="this.media='all'"
-			/>
-			<noscript>
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-				/>
-			</noscript>
 		</Head>
 		{children}
 	</React.Fragment>
