774db3baa9d757c94406d520b07ac17dff990d37
Luan Gjokaj <luan@riangle.com>
2022-03-31 13:52:35 +0200

    chore: update dependencies

--- src/components/ImageWrapper/ImageWrapper.styles.js
+++ src/components/ImageWrapper/ImageWrapper.styles.js
@@ -3,6 +3,7 @@ import { css } from "@emotion/react";
 export const imageWrapperStyles = (theme) => css`
 	display: flex;
 	height: 100%;
+	justify-content: center;
 
 	& > div {
 		margin: auto 0 auto auto !important;
