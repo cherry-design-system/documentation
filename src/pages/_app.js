a29ee8aac55939a8f962e97a310f54b96c049e3c
Luan Gjokaj <luan@riangle.com>
2022-04-01 01:10:47 +0200

    refactor: cleanup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useRouter, Router } from "next/router";
-import Head from "next/head";
 import NProgress from "nprogress";
 import { ThemeProvider } from "@emotion/react";
 import { Col, Container, MinHeight, Row } from "cherry-components";
@@ -12,7 +11,6 @@ import { Footer } from "../components/Footer/Footer";
 import { Sidebar } from "../components/Sidebar";
 import { ScrollToTop } from "../components/ScrollToTop";
 import { ThemeSwitcher } from "../components/ThemeSwitcher/ThemeSwitcher";
-import { OverflowHidden } from "../components/Layout/OverflowHidden";
 
 NProgress.configure({ parent: "#header-inner" });
 Router.events.on("routeChangeStart", () => NProgress.start());
@@ -21,11 +19,9 @@ Router.events.on("routeChangeError", () => NProgress.done());
 
 function App({ Component, pageProps }) {
 	const router = useRouter();
-	const [isLoaded, setIsLoaded] = useState(false);
 	const [currentTheme, setCurrentTheme] = useState(theme);
 
 	useEffect(() => {
-		setIsLoaded(true);
 		if (localStorage.theme === "dark") {
 			document.documentElement.classList.add("dark");
 			setCurrentTheme(themeDark);
@@ -37,29 +33,6 @@ function App({ Component, pageProps }) {
 	return (
 		<ThemeProvider theme={currentTheme}>
 			{globalStyles}
-			<Head>
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossOrigin="true"
-				/>
-				<link
-					rel="preload"
-					as="style"
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-				/>
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-					media={isLoaded ? "all" : "print"}
-				/>
-				<noscript>
-					<link
-						rel="stylesheet"
-						href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-					/>
-				</noscript>
-			</Head>
 			<ScrollToTop>
 				<Header
 					themeSwitcher={<ThemeSwitcher setTheme={setCurrentTheme} />}
@@ -67,21 +40,14 @@ function App({ Component, pageProps }) {
 				<MinHeight theme={currentTheme}>
 					{router.pathname.includes("/docs") ? (
 						<Container theme={currentTheme} fluid>
-							<OverflowHidden>
-								<Row alignItems="flex-start" gutterLg="medium">
-									<Col
-										xs={12}
-										lg={3}
-										sticky
-										theme={currentTheme}
-									>
-										<Sidebar />
-									</Col>
-									<Col xs={12} lg={9} id="doc-content">
-										<Component {...pageProps} />
-									</Col>
-								</Row>
-							</OverflowHidden>
+							<Row alignItems="flex-start">
+								<Col xs={12} lg={3} sticky theme={currentTheme}>
+									<Sidebar />
+								</Col>
+								<Col xs={12} lg={9} id="doc-content">
+									<Component {...pageProps} />
+								</Col>
+							</Row>
 						</Container>
 					) : (
 						<Component {...pageProps} />
