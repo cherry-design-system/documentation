848b378296eccf7390d0c3707bad74ac60d7691b
Luan Gjokaj <luan@riangle.com>
2024-10-24 19:21:40 +0200

    docs: update

--- src/app/components/content.tsx
+++ src/app/components/content.tsx
@@ -63,6 +63,14 @@ const StyledContent = styled.div<ContentProps>`
     }
   }
 
+  & code:not([class]) {
+    display: inline;
+    background: ${({ theme }) => theme.colors.grayLight};
+    border-radius: ${({ theme }) => theme.spacing.radius.xs};
+    border: solid 1px ${({ theme }) => theme.colors.grayDark};
+    font-family: ${({ theme }) => theme.fonts.mono};
+  }
+
   & button,
   & h1,
   & h2,
